69e13ed1a2571fd17a647dba8ad72d971644e93b
Aurelijus Banelis <aurelijus@banelis.lt>
2019-10-22 22:24:53 +0300

    Upgrade: add orm bundle dependency
    This will be used to work with MySql database
    See commands bin/console doctrine:*
    
    Generated with:
      composer require orm
    
    Documentation:
      https://symfony.com/doc/current/doctrine.html

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,7 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];

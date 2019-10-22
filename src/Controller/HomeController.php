f4388b17957cd17e1e94c705c24c393e8263e25c
Aurelijus Banelis <aurelijus@banelis.lt>
2019-10-22 20:58:59 +0300

    Revert "Temporary: Testing GitHub actions integration"
    This reverts commit 9a26428da9439b5ee673efee0f0dc9761c37902a.
    
    Error was caught via https://github.com/aurelijusb/kickstart/commit/9a26428da9439b5ee673efee0f0dc9761c37902a/checks?check_suite_id=276379243

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,8 +10,8 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index() {
-        // Something that violates Code style
+    public function index()
+    {
         return $this->render('home/index.html.twig', [
             'someVariable' => 'NFQ Akademija',
         ]);

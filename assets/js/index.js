54cbb12ee009e567117464c8be0b0132473622bd
Evaldas Burlingis <evabur@icloud.com>
2019-11-03 09:39:47 +0000

    deleted unused import

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-// import '../css/app.css';
 import App from './app';
 
 ReactDOM.render (

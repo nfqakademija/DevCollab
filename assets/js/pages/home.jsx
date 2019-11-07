cfe6b9a58f09954549ab007cad4b69f23e5d8cb5
Evaldas Burlingis <evabur@icloud.com>
2019-11-07 07:03:54 +0000

    landing page desktop + mobile

--- assets/js/pages/home.jsx
+++ assets/js/pages/home.jsx
@@ -6,6 +6,7 @@ import HIW3Img from '../../img/hiw3.png';
 import HIW4Img from '../../img/hiw4.png';
 
 
+
 const CLASSES = {
     main: "poppins w-full",
     hero: {
@@ -16,9 +17,24 @@ const CLASSES = {
         btn: "inline-block text-md lg:text-lg font-semibold bg-teal-500 rounded px-4 py-2 text-white my-2 mr-2 hover:bg-teal-600 cs-shadow-green",
         img: "w-full lg:w-3/5 h-auto"
     },
+    hiw: {
+        section: "w-full landing-bg-gradient pt-8 lg:pt-16 pb-8 relative",
+        title: "text-center text-5xl font-bold tracking-tight mb-4 lg:mb-24",
+        rowContainer: "flex w-full flex-col lg:flex-row mb-8 xl:mb-32 relative",
+        imageLeft: "px-2 lg:px-0 hiw-image-bg hiw-rounded-r -ml-2 lg:ml-0 flex justify-center lg:justify-end items-center",
+        imageRight: "px-2 lg:px-0 hiw-image-bg hiw-rounded-l -ml-2 lg:ml-0 flex justify-center lg:justify-start items-center",
+        content: "w-full mx-auto",
+        contentRight: "order-1 lg:order-2 w-full flex flex-col justify-center mb-8 px-2 lg:px-8 xl:px-64 sm:mb-12",
+        contentLeft: "w-full flex flex-col justify-center mb-8 px-2 lg:px-8 xl:px-64 sm:mb-12",
+        contentTitle: "text-3xl font-semibold text-center lg:text-left text-gray-900",
+        contentLead: "text-lg font-medium text-gray-800"
+    }
 }
 
 const HomePage = () => {
+    // Nine bubbles per colour
+    const bubbles = [1,2,3,4,5,6,7,8,9]
+
     return(
         <main className={CLASSES.main}>
             {/* HERO SECTION */}
@@ -33,7 +49,7 @@ const HomePage = () => {
                     </p>
                     <div>
                         <a className={CLASSES.hero.btn} href="#about">How it works</a>
-                        <a className={CLASSES.hero.btn}>Join the challenge</a>
+                        <a className={CLASSES.hero.btn} href="#">Join the challenge</a>
                     </div>
                 </div>
                 <div className={CLASSES.hero.img}>
@@ -42,64 +58,68 @@ const HomePage = () => {
             </section>
 
             {/* HOW IT WORKS SECTION */}
-            <section className="w-full landing-bg-gradient pt-8 lg:pt-16 pb-8" id="about">
-                <h2 className="text-center text-5xl font-bold tracking-tight mb-4 lg:mb-24">How it works</h2>
+            <section className={CLASSES.hiw.section} id="about">
+                {bubbles.map(id => <span className={`greenBubble${id}`} />)}
+                {bubbles.map(id => <span className={`blueBubble${id}`} />)}
+                {bubbles.map(id => <span className={`yellowBubble${id}`} />)}
 
+
+                <h2 className={CLASSES.hiw.title}>How it works</h2>
                 {/* IT STARTS WITH YOU */}
-                <div className="flex w-full flex-col lg:flex-row mb-8 xl:mb-32">
+                <div className={CLASSES.hiw.rowContainer}>
                     <div className="w-full order-2 lg:order-1">
-                        <div className="px-2 lg:px-0 hiw-image-bg hiw-rounded-r -ml-2 lg:ml-0 flex justify-center lg:justify-end items-center">
+                        <div className={CLASSES.hiw.imageLeft}>
                             <img src={HIW1Img} alt="" className="xl:-mr-8"/>
                         </div>
                     </div>
-                    <div className="order-1 lg:order-2 w-full flex flex-col justify-center mb-4 px-2 lg:px-8 xl:px-64 sm:mb-12">
-                        <div className="w-full mx-auto">
-                            <h3 className="text-3xl font-semibold text-center lg:text-left text-gray-900">It starts with you</h3>
-                            <p className="text-lg font-medium text-gray-800">Once we’ve got information about you, our insanely smart AI will start cheking for other people you can team up with</p>
+                    <div className={CLASSES.hiw.contentRight}>
+                        <div className={CLASSES.hiw.content}>
+                            <h3 className={CLASSES.hiw.contentTitle}>It starts with you</h3>
+                            <p className={CLASSES.hiw.contentLead}>Once we’ve got information about you, our insanely smart AI will start cheking for other people you can team up with</p>
                         </div>
                     </div>
                 </div>
 
                 {/* TEAM HAS BEEN BUILT */}
-                <div className="flex w-full flex-col lg:flex-row">
-                    <div className="w-full flex flex-col justify-center mb-4 px-2 lg:px-8 xl:px-64 sm:mb-12">
-                        <div className="w-full mx-auto">
-                            <h3 className="text-3xl font-semibold text-center lg:text-left text-gray-900">Team has been built</h3>
-                            <p className="text-lg font-medium text-gray-800">After team building will be finished, you will get all the details to contact your teammates. Soon project details will be sent to all of you!</p>
+                <div className={CLASSES.hiw.rowContainer}>
+                    <div className={CLASSES.hiw.contentLeft}>
+                        <div className={CLASSES.hiw.content}>
+                            <h3 className={CLASSES.hiw.contentTitle}>Team has been built</h3>
+                            <p className={CLASSES.hiw.contentLead}>After team building will be finished, you will get all the details to contact your teammates. Soon project details will be sent to all of you!</p>
                         </div>
                     </div>
                     <div className="w-full">
-                        <div className="px-2 lg:px-0 hiw-image-bg hiw-rounded-l -ml-2 lg:ml-0 flex justify-center lg:justify-start items-center">
+                        <div className={CLASSES.hiw.imageRight}>
                             <img src={HIW2Img} alt="" className="xl:-ml-8"/>
                         </div>
                     </div>
                 </div>
 
                 {/* IT STARTS WITH YOU */}
-                <div className="flex w-full flex-col lg:flex-row mb-8 xl:mb-32">
+                <div className={CLASSES.hiw.rowContainer}>
                     <div className="w-full order-2 lg:order-1">
-                        <div className="px-2 lg:px-0 hiw-image-bg hiw-rounded-r -ml-2 lg:ml-0 flex justify-center lg:justify-end items-center">
+                        <div className={CLASSES.hiw.imageLeft}>
                             <img src={HIW3Img} alt="" className="xl:-mr-8"/>
                         </div>
                     </div>
-                    <div className="order-1 lg:order-2 w-full flex flex-col justify-center mb-4 px-2 lg:px-8 xl:px-64 sm:mb-12">
-                        <div className="w-full mx-auto">
-                            <h3 className="text-3xl font-semibold text-center lg:text-left text-gray-900">Working together</h3>
-                            <p className="text-lg font-medium text-gray-800">Your team will receive details about project and it will be up to all of you on how to make it real.</p>
+                    <div className={CLASSES.hiw.contentRight}>
+                        <div className={CLASSES.hiw.content}>
+                            <h3 className={CLASSES.hiw.contentTitle}>Working together</h3>
+                            <p className={CLASSES.hiw.contentLead}>Your team will receive details about project and it will be up to all of you on how to make it real.</p>
                         </div>
                     </div>
                 </div>
 
                 {/* COMPETING AGAINS OTHERS */}
-                <div className="flex w-full flex-col lg:flex-row">
-                    <div className="w-full flex flex-col justify-center mb-4 px-2 lg:px-8 xl:px-64 sm:mb-12">
-                        <div className="w-full mx-auto">
-                            <h3 className="text-3xl font-semibold text-center lg:text-left text-gray-900">Competing against others</h3>
-                            <p className="text-lg font-medium text-gray-800">All teams will be making same project, but it will be up to them on how to make it. Our senior developers will be reviewing and scoring teams on code quality, complexity and other markers.</p>
+                <div className={CLASSES.hiw.rowContainer}>
+                    <div className={CLASSES.hiw.contentLeft}>
+                        <div className={CLASSES.hiw.content}>
+                            <h3 className={CLASSES.hiw.contentTitle}>Competing against others</h3>
+                            <p className={CLASSES.hiw.contentLead}>All teams will be making same project, but it will be up to them on how to make it. Our senior developers will be reviewing and scoring teams on code quality, complexity and other markers.</p>
                         </div>
                     </div>
                     <div className="w-full">
-                        <div className="px-2 lg:px-0 hiw-image-bg hiw-rounded-l -ml-2 lg:ml-0 flex justify-center lg:justify-start items-center">
+                        <div className={CLASSES.hiw.imageRight}>
                             <img src={HIW4Img} alt="" className="xl:-ml-8"/>
                         </div>
                     </div>

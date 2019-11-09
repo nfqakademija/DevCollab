126fe5a61c594550d3e873440fe2312888fc69e0
Montekarl <karolis.petrikas@mail.com>
2019-11-09 21:32:25 +0000

    installed Easy Admin bundle

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];

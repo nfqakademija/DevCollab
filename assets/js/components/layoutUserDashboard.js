6437172f3675e84d31677d8cdc0b7deb76039477
arnas277 <Arnas277@gmail.com>
2019-12-11 20:07:34 +0000

    Fixing bugs

--- assets/js/components/layoutUserDashboard.js
+++ assets/js/components/layoutUserDashboard.js
@@ -55,7 +55,7 @@ const username = localStorage.getItem("user");
         <div className="w-full h-auto my-8 items-center hidden xl:flex">
           <div className="bg-blue-500 h-12 w-12 rounded-full flex justify-center items-center">
             <span className="text-white text-2xl font-semibold">
-              kazkas
+              {username}
             </span>
           </div>
           <div className="ml-2 text-left text-lg font-semibold">

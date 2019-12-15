fde341f97263d9c210937bbae0e6fe87c2ad9ade
algyz200 <algirdas.toleikis@gmail.com>
2019-12-15 14:09:45 +0000

    fixtures hash passwords

--- src/DataFixtures/UsersFixtures.php
+++ src/DataFixtures/UsersFixtures.php
@@ -19,7 +19,7 @@ class UsersFixtures extends Fixture implements DependentFixtureInterface
             $users->setName('firstname-' . $i);
             $users->setLastname('lastname-' . $i);
             $users->setEmail('email' . $i . '@gmail.com');
-            $users->setPassword(mt_rand(10, 10000000));
+            $users->setPassword(password_hash(123456,PASSWORD_ARGON2I));
             $users->setRoles('ROLES_USER');
             $users->setTeam($this->getReference(TeamsFixtures::TEAM_REFERENCE));
             $manager->persist($users);

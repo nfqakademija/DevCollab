af0d3883c976add5cbdef048ae2411eb84b27ce4
algyz200 <algirdas.toleikis@gmail.com>
2019-12-16 21:34:35 +0000

    jointeam input format fixed

--- src/Controller/Api/TeamController.php
+++ src/Controller/Api/TeamController.php
@@ -131,8 +131,8 @@ class TeamController extends AbstractFOSRestController
      */
     public function teamSorter(Request $request): Response
     {
-        $userId = json_decode($request->getContent());
-        $userId = $userId[0];
+        $userId = json_decode($request->getContent(), true);
+        $userId= $userId['id'];
         $teamsArray = $this->showTeams()->getContent();
         $teamstoArray = json_decode($teamsArray, true);
         $randomTeam = array_rand($teamstoArray, 1);
@@ -152,8 +152,6 @@ class TeamController extends AbstractFOSRestController
         $user['teamId'] = $randomTeamConverted;
 
         return $this->handleView($this->view($user));
-
-        return new Response("betkas");
     }
 
 }

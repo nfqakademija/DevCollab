4af518131fae3faa016afa26ddd158047b1c2a02
algyz200 <algirdas.toleikis@gmail.com>
2019-12-17 17:40:19 +0000

    mandatory commit

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -43,15 +43,15 @@ class SecurityController extends AbstractController
 //         // if ($this->getUser()) {
 //         //     return $this->redirectToRoute('target_path');
 //         // }
-
+//
 //         // get the login error if there is one
 //         $error = $authenticationUtils->getLastAuthenticationError();
 //         // last username entered by the user
 //         $lastUsername = $authenticationUtils->getLastUsername();
-
-//         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+//
+//         return new Response("labas");
 //     }
-
+//
 //     /**
 //      * @Route("security/logout", name="app_logout")
 //      */

1e7f4c8097127c734c98c9ff1d063e41daa6c5dc
arnas277 <Arnas277@gmail.com>
2019-12-18 00:56:32 +0000

    fixing

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -43,7 +43,8 @@ class UserRepository extends ServiceEntityRepository
     public function getUsersData(string $username)
     {
         $qb = $this->createQueryBuilder('p')
-            ->select('p.id, p.name, p.lastname, p.location, p.email, p.github_username, p.short_description, p.username')
+            ->select('p.id,
+            p.name, p.lastname, p.location, p.email, p.github_username, p.short_description, p.username')
             ->leftJoin('p.team', 'team')
             ->where('p.username = :user')
             ->setParameter('user', $username);
